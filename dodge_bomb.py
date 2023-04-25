697c71322c490f416c2aacedeffd00e1b8b0798c
Haruki <c0b221768c@edu.teu.ac.jp>
2023-04-25 16:37:45 +0900

    追加機能5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,15 +1,24 @@
 import random
 import sys
-
+import time
+import math
 import pygame as pg
+from tkinter import messagebox
 
-delta = {pg.K_UP : (0,-1),
+move_key_list = {
+         pg.K_UP : (0,-1),
          pg.K_DOWN :(0,+1),
          pg.K_LEFT :(-1,0),
-         pg.K_RIGHT :(+1,0)}
+         pg.K_RIGHT :(+1,0)
+         }
+move_direction_list = {
+    
+}
 
 
-def check_bound(scr_rct:pg.Rect,obj_rct:pg.Rect) -> tuple[bool,bool]:
+
+
+def check_bound(screen_rect:pg.Rect,obj_rect:pg.Rect) -> tuple[bool,bool]:
     """
     オブジェクトが画面内or画面外を判定し、真理値(True,False)を判定し、真理値タプルを返す関数
     引数1:画面SurfaceのRect
@@ -17,12 +26,12 @@ def check_bound(scr_rct:pg.Rect,obj_rct:pg.Rect) -> tuple[bool,bool]:
     戻り値:横方向、縦方向のはみだし判定結果(画面内:True/画面外:False)
     """
     
-    yoko, tate = True, True
-    if obj_rct.left< scr_rct.left or scr_rct.right < obj_rct.right:
-        yoko = False
-    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
-        tate =False
-    return yoko,tate
+    horizontal, vertical = True, True
+    if obj_rect.left< screen_rect.left or screen_rect.right < obj_rect.right:
+        horizontal = False
+    if obj_rect.top < screen_rect.top or screen_rect.bottom < obj_rect.bottom:
+        vertical =False
+    return horizontal,vertical
 
 
 def main():
@@ -32,19 +41,21 @@ def main():
     bg_img = pg.image.load("./fig/pg_bg.jpg")
     kk_img = pg.image.load("./fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rct = kk_img.get_rect()
-    kk_rct.center =900,400
+    kk_rect = kk_img.get_rect()
+    kk_rect.center =900,400
 
 
-    bb_img = pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_img.set_colorkey((0,0,0))
-    x,y = random.randint(0,1200),random.randint(0,900)
-    screen.blit(bb_img,[x,y])
+
+    bomb_img = pg.Surface((20,20))
+    pg.draw.circle(bomb_img,(255,0,0),(10,10),10)
+    bomb_img.set_colorkey((0,0,0))
     vx,vy=+1,+1#練習3
-    bb_rct = bb_img.get_rect()#練習3
-    bb_rct.center =x,y#練習3
+    bomb_rect = bomb_img.get_rect()#練習3
+    bomb_rect.center =random.randint(0, 1200),random.randint(0, 900)#練習3
     tmr = 0#練習3
+
+    # make timer
+    start_timer = time.time()
  
 
     while True:
@@ -55,26 +66,28 @@ def main():
 
 
         key_lst = pg.key.get_pressed()
-        for k,mv in delta.items():
+        for k,mv in move_key_list.items():
             if key_lst[k]:
-                kk_rct.move_ip(mv)
-        if check_bound(screen.get_rect(),kk_rct) != (True,True):
-            for k,mv in delta.items():
+                kk_rect.move_ip(mv)
+        if check_bound(screen.get_rect(),kk_rect) != (True,True):
+            for k,mv in move_key_list.items():
                 if key_lst[k]:
-                    kk_rct.move_ip(-mv[0],-mv[1])    
+                    kk_rect.move_ip(-mv[0],-mv[1])    
 
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rct)
-        # screen.blit(bb_img,[600,200])
-        bb_rct.move_ip(vx,vy)#練習3
-        yoko,tate = check_bound(screen.get_rect(),bb_rct)
-        if not yoko: #横方向にはみ出ていたら
+        screen.blit(kk_img, kk_rect)
+        bomb_rect.move_ip(vx,vy)#練習3
+        horizontal,vertical = check_bound(screen.get_rect(),bomb_rect)
+        if not horizontal: #横方向にはみ出ていたら
             vx *= -1
-        if not tate: #縦方向にはみ出ていたら
+        if not vertical: #縦方向にはみ出ていたら
             vy *= -1
-        screen.blit(bb_img,bb_rct)#練習3
-        if kk_rct.colliderect(bb_rct):#練習6
+        screen.blit(bomb_img,bomb_rect)#練習3
+        if kk_rect.colliderect(bomb_rect):#練習6
+            stop_timer = time.time()
+            result = math.floor(stop_timer-start_timer)
+            messagebox.showinfo('結果発表',str(result)+'秒！')
             return
 
 
